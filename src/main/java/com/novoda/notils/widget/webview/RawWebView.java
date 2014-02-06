6e522d7343b83b76394388b7077b485a8bcd92a6
Antonio Bertucci <archano@gmail.com>
2014-02-06 12:11:49 +0000

    Adds the possibility of loading a raw file using the resource id.

--- src/main/java/com/novoda/notils/widget/webview/RawWebView.java
+++ src/main/java/com/novoda/notils/widget/webview/RawWebView.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.webkit.WebView;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 /**
  * A WebView that allows you to load a html file from your /res/raw directory
  */
@@ -26,8 +31,43 @@ public class RawWebView extends WebView {
         loadDataWithBaseURL(ANDROID_RAW_BASE_URL, html, TEXT_HTML, DEFAULT_ENCODING, FAIL_URL);
     }
 
+    /**
+     * Using this method with Proguard may result it missing file behaviour.
+     * Recommended {@link #loadRawResource(int)} instead.
+     * @param rawFileName a filename in your /raw/ directory i.e. "novoda.html"
+     */
     public void loadRawUrl(String rawFileName) {
         loadUrl(ANDROID_RAW_BASE_URL + "/" + rawFileName);
+    }
+
+    /**
+     *
+     * @param rawResourceId the corresponding id of a filename in your /raw/ directory i.e. "R.raw.novoda"
+     */
+    public void loadRawResource(int rawResourceId) {
+        InputStream input = null;
+        try {
+            input = getResources().openRawResource(rawResourceId);
+            String html = loadFrom(input);
+            loadRawData(html);
+        } finally {
+            tryClose(input);
+        }
+    }
+
+    private String loadFrom(InputStream input) {
+        Scanner scanner = new Scanner(input, DEFAULT_ENCODING).useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
+    }
+
+    private void tryClose(Closeable input) {
+        try {
+            if (input != null) {
+                input.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
