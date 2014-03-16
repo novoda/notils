645493a76111469ded45040a3675a9331e0b2e10
Ataul Munim <timberwo7ves@gmail.com>
2014-03-16 12:12:31 +0000

    Adds setter for enabling/disabling

--- src/main/java/com/novoda/notils/viewserver/ViewServerManager.java
+++ src/main/java/com/novoda/notils/viewserver/ViewServerManager.java
@@ -11,8 +11,8 @@ import android.app.Activity;
  * otherwise, specify this in a separate debug manifest; the ViewServer should
  * only be used during debug anyway.
  *
- * Ensure {@link #on} is set to true if you want to use the ViewServer. This
- * is useful to allow switching off for debug builds.
+ * Ensure {@link #enable(boolean)} is called with true if you want to use the
+ * ViewServer. This is useful to allow switching off for debug builds.
  *
  * Create an instance of the ViewServerManager, and call {@link #onCreate()},
  * {@link #onResume()} and {@link #onDestroy()} in the corresponding methods
@@ -20,28 +20,31 @@ import android.app.Activity;
  */
 public class ViewServerManager {
 
-    public static boolean on;
-
     private Activity activity;
+    private boolean enabled;
 
     public ViewServerManager(Activity activity) {
         this.activity = activity;
     }
 
+    public void enable(boolean enabled) {
+        this.enabled = enabled;
+    }
+
     public void onCreate() {
-        if (on) {
+        if (enabled) {
             ViewServer.get(activity).addWindow(activity);
         }
     }
 
     public void onResume() {
-        if (on) {
+        if (enabled) {
             ViewServer.get(activity).setFocusedWindow(activity);
         }
     }
 
     public void onDestroy() {
-        if (on) {
+        if (enabled) {
             ViewServer.get(activity).removeWindow(activity);
         }
     }
