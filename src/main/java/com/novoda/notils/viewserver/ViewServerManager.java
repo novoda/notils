f6d1472d26a273b7ffb594026ac728d803fd923b
Ataul Munim <timberwo7ves@gmail.com>
2014-03-16 17:51:18 +0000

    Removes gradle assumption from javadoc

--- src/main/java/com/novoda/notils/viewserver/ViewServerManager.java
+++ src/main/java/com/novoda/notils/viewserver/ViewServerManager.java
@@ -7,12 +7,9 @@ import android.app.Activity;
  * devices to use HierarchyViewer.
  *
  * To use this, your application must specify the internet permission in the
- * Android Manifest. If your application does not require internet permissions
- * otherwise, specify this in a separate debug manifest; the ViewServer should
- * only be used during debug anyway.
- *
- * Ensure {@link #enable(boolean)} is called with true if you want to use the
- * ViewServer. This is useful to allow switching off for debug builds.
+ * Android Manifest. Ensure {@link #enable(boolean)} is called with true if
+ * you want to use the ViewServer. This is useful to ensure it only runs in
+ * debug builds.
  *
  * Create an instance of the ViewServerManager, and call {@link #onCreate()},
  * {@link #onResume()} and {@link #onDestroy()} in the corresponding methods
