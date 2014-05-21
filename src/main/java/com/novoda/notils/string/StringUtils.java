567ca70cffb38e893275ab2b5c9918d144f7e2a1
Stefan Hoth <sh@jnamic.com>
2014-05-21 18:21:13 +0100

    Optimize loop

--- src/main/java/com/novoda/notils/string/StringUtils.java
+++ src/main/java/com/novoda/notils/string/StringUtils.java
@@ -7,13 +7,12 @@ public abstract class StringUtils {
 
     public static String join(Collection<?> s, String delimiter) {
         StringBuilder builder = new StringBuilder();
-        Iterator<?> iter = s.iterator();
-        while (iter.hasNext()) {
-            builder.append(iter.next());
-            if (!iter.hasNext()) {
-                break;
-            }
-            builder.append(delimiter);
+        Iterator<?> it = s.iterator();
+        String separator = "";
+
+        while (it.hasNext()) {
+            builder.append(separator).append(it.next());
+            separator = delimiter; //use delimiter from the 2nd time on
         }
         return builder.toString();
     }
