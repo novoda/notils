c0ba6c83bddd05e64d12cb57899dc1ad361c0119
Stefan Hoth <sh@jnamic.com>
2014-05-21 18:57:13 +0100

    Add check and handling for null values, optimize loop further, add more tests for new cases, javadoc

--- src/main/java/com/novoda/notils/string/StringUtils.java
+++ src/main/java/com/novoda/notils/string/StringUtils.java
@@ -5,15 +5,31 @@ import java.util.Iterator;
 
 public abstract class StringUtils {
 
-    public static String join(Collection<?> s, String delimiter) {
+    private final static String EMPTY_STRING = "";
+
+    /**
+     * Joins a Collection of typed objects using their toString method, separated by delimiter
+     * @param collection Collection of objects
+     * @param delimiter (optional) String to separate the items of the collection in the joined string - <code>null</code> is interpreted as empty string
+     * @return the joined string
+     */
+    public static String join(Collection<?> collection, String delimiter) {
+
+        if (collection == null || collection.isEmpty()) {
+            return EMPTY_STRING;
+        } else if (delimiter == null) {
+            delimiter = EMPTY_STRING;
+        }
+
         StringBuilder builder = new StringBuilder();
-        Iterator<?> it = s.iterator();
-        String separator = "";
+        Iterator<?> it = collection.iterator();
 
         while (it.hasNext()) {
-            builder.append(separator).append(it.next());
-            separator = delimiter; //use delimiter from the 2nd time on
+            builder.append(it.next()).append(delimiter);
         }
+
+        int length = builder.toString().length();
+        builder.delete(length - delimiter.length(), length);
         return builder.toString();
     }
 }
--- src/test/java/com/novoda/notils/string/StringUtilsTest.java
+++ src/test/java/com/novoda/notils/string/StringUtilsTest.java
@@ -1,9 +1,9 @@
 package com.novoda.notils.string;
 
-import junit.framework.TestCase;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import junit.framework.TestCase;
 
 public class StringUtilsTest extends TestCase {
 
@@ -20,7 +20,7 @@ public class StringUtilsTest extends TestCase {
         strings.add(string3);
 
         String expected = string1+delim+string2+delim+string3;
-        assertEquals(expected, StringUtils.join(strings, delim));
+        assertEquals("Strings get concatenated with delimiter", expected, StringUtils.join(strings, delim));
 
 
         string1 = "a";
@@ -36,12 +36,37 @@ public class StringUtilsTest extends TestCase {
         strings.add(string4);
 
         expected = string1+delim+string2+delim+string3+delim+string4;
+        assertEquals("Strings get concatenated with 2-char-delimiter", expected, StringUtils.join(strings, delim));
+
+        strings = null;
+        delim = ";";
+        expected = "";
         assertEquals(expected, StringUtils.join(strings, delim));
+
+        strings = new ArrayList<String>(0);
+        delim = ";";
+        expected = "";
+        assertEquals("Joining collection=null returns empty string", expected, StringUtils.join(strings, delim));
 
         strings = new ArrayList<String>(0);
         delim   = ";";
         expected = "";
-        assertEquals(expected, StringUtils.join(strings, delim));
+        assertEquals("Joining empty collection returns empty string", expected, StringUtils.join(strings, delim));
+
+        string1 = "a";
+        string2 = "story";
+        string3 = "about";
+        string4 = "missing delimiters";
+        delim   = null;
+
+        strings = new ArrayList<String>(3);
+        strings.add(string1);
+        strings.add(string2);
+        strings.add(string3);
+        strings.add(string4);
+
+        expected = string1+string2+string3+string4;
+        assertEquals("Delimiter=null is interpreted as empty string", expected, StringUtils.join(strings, delim));
 
         Integer integer1 = 1;
         Integer integer2 = 2;
@@ -55,7 +80,7 @@ public class StringUtilsTest extends TestCase {
         integers.add(integer4);
 
         expected = integer1.toString()+delim+integer2.toString()+delim+integer3.toString()+delim+integer4.toString();
-        assertEquals(expected, StringUtils.join(integers, delim));
+        assertEquals("Collection of Integer will be joined properly using the toString-method", expected, StringUtils.join(integers, delim));
 
     }
 }
