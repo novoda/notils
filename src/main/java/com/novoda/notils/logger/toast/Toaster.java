a6215c85567cb36f4653591dc012ee82693910a7
Ataul Munim <timberwo7ves@gmail.com>
2014-06-05 16:09:39 +0100

    Adds static factory methods in Toaster
    - removes the extra implementation
    - adds static factory methods for user to request Toaster with specific behaviour

--- src/main/java/com/novoda/notils/logger/toast/Toaster.java
+++ src/main/java/com/novoda/notils/logger/toast/Toaster.java
@@ -6,26 +6,47 @@ import android.widget.Toast;
 import com.novoda.notils.logger.simple.Log;
 
 /**
- * A Toast helper giving a short hand to show toasts.
- * Also checks for the validity of your context and Log's if it cannot toast.
+ * Toast helper providing a short hand to display Toast messages.
+ * <p/>
+ * If the Toast cannot be shown, it will display an error log.
  */
 public class Toaster {
 
     private final Context context;
+    private final boolean cancelCurrentBeforeShowingNewToast;
+
+    private Toast toast;
+
+    public static Toaster from(Context context) {
+        return new Toaster(context);
+    }
+
+    public static Toaster nonStackingFrom(Context context) {
+        return new Toaster(context, true);
+    }
 
     /**
-     * A helper in Toasting messages to the screen
+     * Returns new Toaster helper.
      *
      * @param context
+     * @deprecated this ctor will be made private at the next major release.
+     * Use Toaster.from(Context) or Toaster.nonStackingFrom(Context) instead.
      */
+    @Deprecated
     public Toaster(Context context) {
         this.context = context;
+        this.cancelCurrentBeforeShowingNewToast = false;
+    }
+
+    private Toaster(Context context, boolean cancelCurrentBeforeShowingNewToast) {
+        this.context = context;
+        this.cancelCurrentBeforeShowingNewToast = cancelCurrentBeforeShowingNewToast;
     }
 
     /**
      * Toast.LENGTH_SHORT
      *
-     * @param msgId
+     * @param msgId String resource id for message
      */
     public void popToast(int msgId) {
         popToast(msgId, Toast.LENGTH_SHORT);
@@ -34,7 +55,7 @@ public class Toaster {
     /**
      * Toast.LENGTH_SHORT
      *
-     * @param msg
+     * @param msg message to display
      */
     public void popToast(String msg) {
         popToast(msg, Toast.LENGTH_SHORT);
@@ -43,7 +64,7 @@ public class Toaster {
     /**
      * Toast.LENGTH_LONG
      *
-     * @param msgId
+     * @param msgId String resource id for message
      */
     public void popBurntToast(int msgId) {
         popToast(msgId, Toast.LENGTH_LONG);
@@ -52,23 +73,39 @@ public class Toaster {
     /**
      * Toast.LENGTH_LONG
      *
-     * @param msg
+     * @param msg message to display
      */
     public void popBurntToast(String msg) {
         popToast(msg, Toast.LENGTH_LONG);
     }
 
     private void popToast(String msg, int length) {
+        if (cancelCurrentBeforeShowingNewToast) {
+            cancelOldToast();
+        }
+
         if (context != null) {
-            Toast.makeText(context, msg, length).show();
+            toast = Toast.makeText(context, msg, length);
+            toast.show();
         } else {
-            Log.e("Couldn't toast, context has become null. Attempted msg: " + msg);
+            Log.e("Unable to toast message (context is null). Message: " + msg);
         }
     }
 
     private void popToast(int msgId, int length) {
+        if (cancelCurrentBeforeShowingNewToast) {
+            cancelOldToast();
+        }
+
         if (context != null) {
-            Toast.makeText(context, msgId, length).show();
+            toast = Toast.makeText(context, msgId, length);
+            toast.show();
+        }
+    }
+
+    private void cancelOldToast() {
+        if (toast != null) {
+            toast.cancel();
         }
     }
 
