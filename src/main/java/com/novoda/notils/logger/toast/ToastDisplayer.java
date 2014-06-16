be259e3612dc417935a39caa6156b4e726f93d01
Ataul Munim <timberwo7ves@gmail.com>
2014-06-16 18:33:31 +0100

    Adds javadoc for the ToastDIsplayer interface

--- src/main/java/com/novoda/notils/logger/toast/ToastDisplayer.java
+++ src/main/java/com/novoda/notils/logger/toast/ToastDisplayer.java
@@ -1,15 +1,41 @@
 package com.novoda.notils.logger.toast;
 
+/**
+ * Displays Android Toast notifications
+ */
 public interface ToastDisplayer {
 
+    /**
+     * Displays Toast for the standard Toast.LENGTH_SHORT period
+     *
+     * @param message
+     */
     void display(String message);
 
+    /**
+     * Displays Toast for the standard Toast.LENGTH_SHORT period
+     *
+     * @param resId
+     */
     void display(int resId);
 
+    /**
+     * Displays Toast for the longer Toast.LENGTH_LONG period
+     *
+     * @param message
+     */
     void displayLong(String message);
 
+    /**
+     * Displays Toast for the longer Toast.LENGTH_LONG period
+     *
+     * @param resId
+     */
     void displayLong(int resId);
 
+    /**
+     * Cancels any Toast that is currently showing, or is waiting to be shown.
+     */
     void cancelAll();
 
 }
