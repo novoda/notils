ee8f3ef1c7f214562810de12057e8dbad51101e4
Ataul Munim <timberwo7ves@gmail.com>
2014-08-28 10:13:08 +0100

    Revert changes to StrictModeManager
    - we need to first update the project such that it can use new apis
    - it's currently depending on a version of the android jar from maven

--- src/main/java/com/novoda/notils/logger/analyse/StrictModeManager.java
+++ src/main/java/com/novoda/notils/logger/analyse/StrictModeManager.java
@@ -1,6 +1,5 @@
 package com.novoda.notils.logger.analyse;
 
-import android.os.Build;
 import android.os.StrictMode;
 
 /**
@@ -24,18 +23,9 @@ public final class StrictModeManager {
      * Initializes StrictMode to close activity and log to console when a violation occurs.
      */
     public static void initializeStrictMode() {
-        initializeStrictMode(newThreadPolicyBuilderWithDefaultViolations(), newVmPolicyBuilderWithDefaultViolations());
-    }
-
-    /**
-     * Initializes StrictMode to close activity and log to console when a violation occurs.
-     *
-     * Allows specification of violations via parameters. Note, penaltyLog and penaltyDeath will be applied.
-     */
-    public static void initializeStrictMode(StrictMode.ThreadPolicy.Builder threadPolicyBuilder, StrictMode.VmPolicy.Builder vmPolicyBuilder) {
         if (ON) {
-            StrictMode.setThreadPolicy(threadPolicyBuilder.penaltyLog().penaltyDeath().build());
-            StrictMode.setVmPolicy(vmPolicyBuilder.penaltyLog().penaltyDeath().build());
+            StrictMode.setThreadPolicy(newThreadPolicyBuilderWithDefaults().penaltyDeath().build());
+            StrictMode.setVmPolicy(newVmPolicyBuilderWithDefaults().penaltyDeath().build());
         }
     }
 
@@ -43,32 +33,18 @@ public final class StrictModeManager {
      * Initializes StrictMode to log to console when a violation occurs.
      */
     public static void initializeStrictModeLogOnly() {
-        initializeStrictModeWithPenaltyLog(newThreadPolicyBuilderWithDefaultViolations(), newVmPolicyBuilderWithDefaultViolations());
-    }
-
-    /**
-     * Initializes StrictMode to log to console when a violation occurs.
-     * <p/>
-     * Allows specification of violations via parameters. Note, penaltyLog will be applied.
-     */
-    public static void initializeStrictModeWithPenaltyLog(StrictMode.ThreadPolicy.Builder threadPolicyBuilder, StrictMode.VmPolicy.Builder vmPolicyBuilder) {
         if (ON) {
-            StrictMode.setThreadPolicy(threadPolicyBuilder.penaltyLog().build());
-            StrictMode.setVmPolicy(vmPolicyBuilder.penaltyLog().build());
+            StrictMode.setThreadPolicy(newThreadPolicyBuilderWithDefaults().build());
+            StrictMode.setVmPolicy(newVmPolicyBuilderWithDefaults().build());
         }
     }
 
-    private static StrictMode.VmPolicy.Builder newVmPolicyBuilderWithDefaultViolations() {
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            builder.detectLeakedRegistrationObjects();
-        }
-        return builder;
+    private static StrictMode.VmPolicy.Builder newVmPolicyBuilderWithDefaults() {
+        return new StrictMode.VmPolicy.Builder().detectAll().penaltyLog();
     }
 
-    private static StrictMode.ThreadPolicy.Builder newThreadPolicyBuilderWithDefaultViolations() {
-        return new StrictMode.ThreadPolicy.Builder()
-                .detectCustomSlowCalls();
+    private static StrictMode.ThreadPolicy.Builder newThreadPolicyBuilderWithDefaults() {
+        return new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog();
     }
 
 }
