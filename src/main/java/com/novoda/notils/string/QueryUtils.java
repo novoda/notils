a18c704d1f7ca33ee254570df9349ae5ea9691cb
Daniele Bonaldo <dany.bony@gmail.com>
2015-07-22 15:35:03 +0100

    Remove query from method name and specify comment

--- src/main/java/com/novoda/notils/string/QueryUtils.java
+++ src/main/java/com/novoda/notils/string/QueryUtils.java
@@ -6,12 +6,12 @@ public class QueryUtils {
 
     /**
      * Creates a string to be used as a placeholder in {@link android.content.ContentResolver} operations selection.
-     * This will allow to use more selection arguments and have them replaced.
+     * This will allow to use more selection arguments and have them replaced when using the IN operator.
      *
      * @param size The number of selection arguments
      * @return Example: for size == 3 : "?, ?, ?"
      */
-    public static String createQuerySelectionPlaceholdersOfSize(int size) {
+    public static String createSelectionPlaceholdersOfSize(int size) {
         String[] questionMarks = new String[size];
         for (int i = 0; i < size; i++) {
             questionMarks[i] = "?";
