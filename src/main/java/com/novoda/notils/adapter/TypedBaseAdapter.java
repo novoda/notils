60827e466f0cc9c5add376474a56e1077bd10ff6
Adam Brown <adam@novoda.com>
2016-02-27 13:30:46 +0000

    deleting the adapter package

--- src/main/java/com/novoda/notils/adapter/TypedBaseAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.novoda.notils.adapter;
-
-import android.widget.BaseAdapter;
-
-public abstract class TypedBaseAdapter<T> extends BaseAdapter {
-
-    @Override
-    public abstract T getItem(int position);
-}
