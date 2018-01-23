5b7534d03c11a2ea7ebc3113d7618dbf4fae35c4
Said Tahsin Dane <tahsin@novoda.com>
2018-01-23 14:31:36 +0100

    Add all versions of Android to AndroidVersion class

--- android/src/main/java/com/novoda/notils/devicedetection/AndroidVersion.java
+++ android/src/main/java/com/novoda/notils/devicedetection/AndroidVersion.java
@@ -24,6 +24,18 @@ public class AndroidVersion {
         return osApiLevel >= Build.VERSION_CODES.JELLY_BEAN;
     }
 
+    public boolean is17JellyBean() {
+        return osApiLevel == Build.VERSION_CODES.JELLY_BEAN_MR1;
+    }
+
+    public boolean is17JellyBeanOrOver() {
+        return osApiLevel >= Build.VERSION_CODES.JELLY_BEAN_MR1;
+    }
+
+    public boolean is18JellyBean() {
+        return osApiLevel == Build.VERSION_CODES.JELLY_BEAN_MR2;
+    }
+
     public boolean is18JellyBeanOrOver() {
         return osApiLevel >= Build.VERSION_CODES.JELLY_BEAN_MR2;
     }
@@ -36,20 +48,44 @@ public class AndroidVersion {
         return osApiLevel >= Build.VERSION_CODES.KITKAT;
     }
 
+    public boolean is21Lollipop() {
+        return osApiLevel == Build.VERSION_CODES.LOLLIPOP;
+    }
+
     public boolean is21LollipopOrOver() {
         return osApiLevel >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    public boolean is22Lollipop() {
+        return osApiLevel == Build.VERSION_CODES.LOLLIPOP_MR1;
+    }
+
+    public boolean is22LollipopOrOver() {
+        return osApiLevel >= Build.VERSION_CODES.LOLLIPOP_MR1;
     }
 
     public boolean isPre21Lollipop() {
         return !is21LollipopOrOver();
     }
 
+    public boolean is23Marshmallow() {
+        return osApiLevel == Build.VERSION_CODES.M;
+    }
+
     public boolean is23MarshmallowOrOver() {
         return osApiLevel >= Build.VERSION_CODES.M;
     }
 
+    public boolean is24Nougat() {
+        return osApiLevel == Build.VERSION_CODES.N;
+    }
+
     public boolean is24NougatOrOver() {
         return osApiLevel >= Build.VERSION_CODES.N;
+    }
+
+    public boolean is26Oreo() {
+        return osApiLevel == Build.VERSION_CODES.O;
     }
 
     public boolean is26OreoOrOver() {
