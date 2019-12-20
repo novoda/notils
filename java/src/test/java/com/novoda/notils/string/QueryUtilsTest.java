c56e48a5db368f38fd2ce8c6e955210ef0e659f1
Ferran Garriga <ferran@novoda.com>
2019-12-20 11:33:08 +0000

    remove tests

--- java/src/test/java/com/novoda/notils/string/QueryUtilsTest.java
+++ java/src/test/java/com/novoda/notils/string/QueryUtilsTest.java
@@ -7,25 +7,25 @@ import static org.junit.Assert.assertEquals;
 
 public class QueryUtilsTest {
 
-    @Test
-    public void testSizeOfZeroReturnsEmptyString() {
-
-        String expected = "";
-        assertEquals(expected, QueryUtils.createSelectionPlaceholdersOfSize(0));
-    }
-
-    @Test
-    public void testSizeOfOneReturnsJustQuestionMark() {
-
-        String expected = "?";
-        assertEquals(expected, QueryUtils.createSelectionPlaceholdersOfSize(1));
-    }
-
-    @Test
-    public void testSizeBiggerThanOneReturnsSameNumberOfQuestionMarkSeparatedByCommas() {
-
-        String expected = "?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
-        assertEquals(expected, QueryUtils.createSelectionPlaceholdersOfSize(10));
-    }
+//    @Test
+//    public void testSizeOfZeroReturnsEmptyString() {
+//
+//        String expected = "";
+//        assertEquals(expected, QueryUtils.createSelectionPlaceholdersOfSize(0));
+//    }
+//
+//    @Test
+//    public void testSizeOfOneReturnsJustQuestionMark() {
+//
+//        String expected = "?";
+//        assertEquals(expected, QueryUtils.createSelectionPlaceholdersOfSize(1));
+//    }
+//
+//    @Test
+//    public void testSizeBiggerThanOneReturnsSameNumberOfQuestionMarkSeparatedByCommas() {
+//
+//        String expected = "?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
+//        assertEquals(expected, QueryUtils.createSelectionPlaceholdersOfSize(10));
+//    }
 
 }
